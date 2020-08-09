67c0bf6b768da802b3111bcbb98587f84cea5a88
victor <victorsz33@hotmail.com>
2020-08-09 19:34:05 -0300

    primeiro programa

---
+++ testezinho.py
@@ -0,0 +1,140 @@
+constituicao = {
+    'pergunta 1': {
+        'pergunta': 'Segundo a Constituição Federal, são direitos dos trabalhadores urbanos e rurais, além de outros que visem à melhoria de sua condição social: ',
+        'respostas': {'a': 'Remuneração do serviço extraordinário superior, no mínimo, em 100% à do normal.',
+                      'b': 'Piso salarial proporcional à extensão e à complexidade do trabalho.',
+                      'c': 'Proibição do trabalho noturno, perigoso ou insalubre a menores de vinte e um anos e de qualquer trabalho a menores de dezoito anos, salvo na condição de aprendiz, a partir de doze anos.',
+                      'd': 'Jornada de oito horas para o trabalho realizado em turnos ininterruptos de revezamento, salvo negociação coletiva.',
+                      'e': 'Seguro-desemprego, em caso de desemprego voluntário.'},
+        'resposta_certa': 'b'
+    },
+
+    'pergunta 2': {
+        'pergunta': 'Edineia, trabalhadora urbana, foi dispensada sem justa causa de seu emprego e entende que possui créditos resultantes dessa relação de trabalho. Em conformidade com a Constituição Federal de 1988, Edineia tem direito de propor ação, em face de seu ex-empregador, ',
+        'respostas': {'a': 'com prazo prescricional de cinco anos, até o limite de dois anos após a extinção do contrato de trabalho.',
+                      'b': 'com prazo prescricional de dez anos, até o limite de cinco anos após a extinção do contrato de trabalho.',
+                      'c': 'a qualquer tempo, uma vez que o direito ao trabalho é um direito fundamental e, portanto, imprescritível.',
+                      'd': 'com prazo prescricional de dez anos, até o limite de dois anos após a extinção do contrato de trabalho.',
+                      'e': 'com prazo prescricional de cinco anos, até o limite de cinco anos após a extinção do contrato de trabalho.'},
+        'resposta_certa': 'a'
+    },
+
+    'pergunta 3': {
+        'pergunta': 'Depois de um longo período de desemprego, José da Silva foi contratado pela empresa Lar Doce Lar Ltda. para trabalhar como vendedor em uma loja de materiais de construção. Dentre os direitos sociais previstos na Constituição Federal, José da Silva fará jus ',
+        'respostas': {'a': 'à participação nos lucros, ou resultados, vinculada à sua remuneração, e, excepcionalmente, participação na gestão da empresa, conforme definido em lei.',
+                      'b': 'à duração do trabalho normal não superior a oito horas diárias e quarenta e oito semanais, facultada a compensação de horários e a redução da jornada, mediante acordo ou convenção coletiva de trabalho.',
+                      'c': 'ao seguro contra acidentes de trabalho, a cargo do empregador, sem excluir a indenização a que este está obrigado, quando incorrer em dolo ou culpa.',
+                      'd': 'à assistência gratuita aos filhos e dependentes desde o nascimento até 6 anos de idade em creches e pré-escolas.',
+                      'e': 'ao aviso prévio proporcional ao tempo de serviço, sendo no mínimo de trinta e cinco dias, nos termos da lei.'},
+        'resposta_certa': 'c'
+    },
+
+    'pergunta 4': {
+        'pergunta': '''Considere:
+
+I. Seguro-desemprego, em caso de desemprego voluntário ou involuntário.
+
+II. Irredutibilidade do salário, salvo o disposto em convenção ou acordo coletivo.
+
+III. Gozo de férias anuais remuneradas com, pelo menos, um terço a mais do que o salário normal.
+
+IV. Aviso prévio proporcional ao tempo de serviço, sendo no máximo de trinta dias, nos termos da lei.
+
+Em conformidade com a Constituição Federal, são direitos dos trabalhadores urbanos e rurais, além de outros que visem à melhoria de sua condição social, aqueles contidos em''',
+        'respostas': {'a': 'III e IV, apenas.',
+                      'b': 'I, II, III e IV.',
+                      'c': 'I e IV, apenas.',
+                      'd': 'I e II, apenas.',
+                      'e': 'II e III, apenas.'},
+        'resposta_certa': 'e'
+    },
+
+    'pergunta 5': {
+        'pergunta': ' Segundo previsto na Constituição Federal de 1988, são considerados direitos sociais',
+        'respostas': {'a': 'a educação, a moradia e a segurança.',
+                      'b': 'a manifestação do pensamento, a propriedade e a herança.',
+                      'c': 'o voto, o registro civil e o lazer.',
+                      'd': 'a previdência social, o sigilo de correspondência e a inviolabilidade do domicílio.',
+                      'e': 'a locomoção dentro do território, a moradia e a presunção de inocência.'},
+        'resposta_certa': 'a'
+    },
+
+    'pergunta 6': {
+        'pergunta': 'São direitos dos trabalhadores urbanos e rurais, instituídos pela Constituição Federal, além de outros que visem à melhoria de sua condição social,',
+        'respostas': {'a': 'irredutibilidade do salário, ainda que haja autorização disposta em convenção ou acordo coletivo.',
+                      'b': 'participação nos lucros, ou resultados, vinculada à remuneração, e, excepcionalmente, participação na gestão da empresa, conforme definido em lei.',
+                      'c': 'licença à gestante, sem prejuízo do emprego e do salário, com a duração de 180 dias.',
+                      'd': 'igualdade de direitos entre o trabalhador com vínculo empregatício permanente e o trabalhador avulso.',
+                      'e': 'seguro contra acidentes de trabalho, a cargo do Estado, sem excluir a indenização a que este está obrigado, quando incorrer em dolo ou culpa.'},
+        'resposta_certa': 'd'
+    },
+}
+
+penal = {
+    'pergunta 1': {
+        'pergunta': 'Não há crime sem lesão efetiva ou ameaça concreta ao bem jurídico tutelado. Tal enunciado refere-se ao princípio da',
+        'respostas': {'a': 'proporcionalidade.',
+                      'b': 'intervenção mínima.',
+                      'c': 'ofensividade.',
+                      'd': 'bagatela imprópria.',
+                      'e': 'alteridade.'},
+        'resposta_certa': 'c'
+    },
+
+    'pergunta 2': {
+        'pergunta': 'Fausto foi condenado por sentença transitada em julgado por crime cometido em 2010, encontrando-se em cumprimento da pena de 10 anos. Em 2015, entrou em vigor uma lei que não mais considera como crime a conduta que levou Fausto à prisão. Neste caso, Fausto',
+        'respostas': {'a': 'será beneficiado pela nova lei, pois a lei penal retroage.',
+                      'b': 'não será beneficiado pela nova lei, pois a Constituição Federal garante a irretroatividade da lei penal.',
+                      'c': 'será beneficiado pela nova lei apenas se esta favorecer ao menos 10 condenados.',
+                      'd': 'não será beneficiado pela nova lei, pois a Constituição garante a retroatividade apenas da lei civil.',
+                      'e': 'não será beneficiado pois a nova lei não pode prejudicar a coisa julgada.'},
+        'resposta_certa': 'a'
+    },
+
+    'pergunta 3': {
+        'pergunta': '',
+        'respostas': {'a': '',
+                      'b': '',
+                      'c': '',
+                      'd': '',
+                      'e': ''},
+        'resposta_certa': 'b'
+    },
+}
+
+while True:
+    tipom = int(input('''Qual materia ?
+[0] penal
+[1] constituição
+'''))
+    if tipom == 0:
+        materia = penal
+        break
+    elif tipom == 1:
+        materia = constituicao
+        break
+
+respostas_certas = 0
+
+for pk, pv in materia.items():
+    print(f'{pk}: {pv["pergunta"]}')
+
+    print('respostas: ')
+    for rk, rv in pv["respostas"].items():
+        print(f'[{rk}]: {rv}')
+
+    resposta_usuario = input('Sua resposta? ').lower().strip()
+
+    if resposta_usuario == pv["resposta_certa"]:
+        print('\033[1;32mvoce acertou\033[m')
+        respostas_certas += 1
+    else:
+        print('\033[1;31mvoce errou\033[m')
+
+    print()
+
+tot_perguntas = len(materia)
+porcentagem_acerto = respostas_certas / tot_perguntas * 100
+
+print(f'voce acertou {respostas_certas} perguntas')
+print(f'sua porcentagem de acerto foi de {porcentagem_acerto}%')
